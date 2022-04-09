16f5a5954731d78d7a8eecab12dd8c21e9801bd0
brianhanson <brian@brianhanson.net>
2022-04-09 12:37:59 -0500

    [WIP] fully generate partials

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,79 @@
+import fs from 'fs';
 import { defineConfig, loadEnv } from 'vite';
 import viteRestart from 'vite-plugin-restart';
-import craftPartials from './vite-plugin-craft-partials.js';
 import sassGlobImports from 'vite-plugin-sass-glob-import';
+import { parse } from 'node-html-parser';
 
 // https://vitejs.dev/config/
 
+function parseFile(html) {
+  const root = parse(html);
+  const scripts = root.querySelectorAll('script');
+  const links = root.querySelectorAll('link');
+  const meta = root.querySelectorAll('meta');
+
+  // eslint-disable-next-line no-console
+  scripts.forEach((script) => {
+    const initialSrc = script.getAttribute('src');
+    const newSrc = initialSrc.replace('./', 'http://localhost:3300/');
+    script.setAttribute('src', newSrc);
+  });
+
+  links.forEach((link) => {
+    const initialValue = link.getAttribute('href');
+    const newValue = initialValue.replace('./', 'http://localhost:3300/');
+    link.setAttribute('href', newValue);
+  });
+
+  return {
+    scripts,
+    links,
+    meta,
+  };
+}
+
+/** TODO: Make option */
+const template = ({ scripts, links, meta }) => {
+  return `
+{% html at head %}
+<script type="module" src="http://localhost:3300/@vite/client"></script>
+${meta.toString()}
+${links.toString()}
+{% endhtml %}
+{% html at endBody %}${scripts.toString()}{% endhtml %}
+`;
+};
+
+/** TODO: Make option */
+const outputFile = './templates/_partials/vite.twig';
+
+function twigTest(options = {}) {
+  let config = null;
+  return {
+    name: 'twig:test',
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
+    buildStart({ input }) {
+      const inputFile = fs.readFileSync(input);
+      const { scripts, links, meta } = parseFile(inputFile.toString());
+      // eslint-disable-next-line no-console
+      console.log({ config });
+      fs.writeFileSync(outputFile, template({ scripts, links, meta }));
+    },
+    transformIndexHtml(html) {
+      const { scripts, links, meta } = parseFile(html);
+      fs.writeFileSync(outputFile, template({ scripts, links, meta }));
+    },
+  };
+}
+
 export default defineConfig(({ command, mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  
+
   return {
-    base: '/dist/',
-    publicDir: './web/',
+    base: command === 'serve' ? '' : '/dist/',
+    publicDir: './web/dist',
     server: {
       port: process.env.VITE_DEV_PORT || 3000,
     },
@@ -18,17 +81,13 @@ export default defineConfig(({ command, mode }) => {
       emptyOutDir: true,
       manifest: true,
       outDir: './web/dist/',
-      assetsDir: './web/static/',
       rollupOptions: {
-        input: {
-          scripts: './src/scripts/main.js',
-          styles: './src/styles/main.scss',
-        },
+        input: './src/index.html',
       },
     },
     plugins: [
+      twigTest(),
       sassGlobImports(),
-      craftPartials(),
       viteRestart({
         reload: ['./templates/**/*'],
       }),

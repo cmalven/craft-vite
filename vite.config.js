62dae58ecb7e033b8d3e38bfebcf5aaf511f3a50
brianhanson <brian@brianhanson.net>
2022-04-09 14:26:51 -0500

    Cleanup serve & build process

--- vite.config.js
+++ vite.config.js
@@ -1,86 +1,9 @@
-import fs from 'fs';
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import viteRestart from 'vite-plugin-restart';
 import sassGlobImports from 'vite-plugin-sass-glob-import';
-import { parse } from 'node-html-parser';
+import craftPartials from './vite-plugin-craft-partials';
 
 // https://vitejs.dev/config/
-
-function parseFile(html) {
-  const root = parse(html);
-  const scripts = root.querySelectorAll('script');
-  const links = root.querySelectorAll('link');
-  const meta = root.querySelectorAll('meta');
-
-  // eslint-disable-next-line no-console
-  scripts.forEach((script) => {
-    const initialSrc = script.getAttribute('src');
-    const newSrc = initialSrc.replace('./', 'http://localhost:3300/');
-    script.setAttribute('src', newSrc);
-  });
-
-  links.forEach((link) => {
-    const initialValue = link.getAttribute('href');
-    const newValue = initialValue.replace('./', 'http://localhost:3300/');
-    link.setAttribute('href', newValue);
-  });
-
-  return {
-    scripts,
-    links,
-    meta,
-  };
-}
-
-function twigTest(options = {}) {
-  const { outputFile, template } = Object.assign(
-    {},
-    {
-      outputFile: './templates/_partials/vite.twig',
-      template({ scripts, links, meta }) {
-        return `
-{% html at head %}${meta.toString()}${links.toString()}{% endhtml %}
-{% html at endBody %}${scripts.toString()}{% endhtml %}
-`;
-      },
-    },
-    options,
-  );
-
-  let config = null;
-  return {
-    name: 'twig:test',
-    configResolved(resolvedConfig) {
-      config = resolvedConfig;
-    },
-    buildStart({ input }) {
-      const { mode } = config;
-      if (mode === 'production') {
-        return;
-      }
-
-      const inputFile = fs.readFileSync(input);
-      const { scripts, links, meta } = parseFile(inputFile.toString());
-
-      fs.writeFileSync(outputFile, template({ scripts, links, meta }));
-    },
-    transformIndexHtml(html, ctx) {
-      const { mode } = config;
-      if (mode !== 'production') {
-        return;
-      }
-
-      const { scripts, links, meta } = parseFile(html);
-      fs.writeFileSync(outputFile, template({ scripts, links, meta }));
-    },
-    closeBundle() {
-      console.log('Removing src files in dist ...');
-      fs.rmSync(path.resolve(config.publicDir, './src'), { recursive: true, force: true });
-    },
-  };
-}
-
 export default defineConfig(({ command, mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
@@ -95,11 +18,11 @@ export default defineConfig(({ command, mode }) => {
       manifest: true,
       outDir: './web/dist/',
       rollupOptions: {
-        input: './src/index.html',
+        input: './src/entry.html',
       },
     },
     plugins: [
-      twigTest(),
+      craftPartials(),
       sassGlobImports(),
       viteRestart({
         reload: ['./templates/**/*'],
